e55cff4d85060e5355ebcb21156900e9f9b36adf
vikram rajpoot <ios.vikram@gmail.com>
2018-12-24 18:35:59 +0530

    Added test caes

--- calculator/src/Calculator.test.js
+++ calculator/src/Calculator.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Calculator from './Modules/Calculator';
 
 import {configure,mount,shallow,render} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
@@ -8,11 +8,14 @@ configure({adapter:new Adapter()})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Calculator />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 test("validate calculator component",()=>{
-    const calculatorCopm = shallow(<App/>)
-    expect(calculatorCopm).toMatchSnapshot()
+    const calculatorCopm = shallow(<Calculator/>)
+    // expect(calculatorCopm).toMatchSnapshot()
+    expect(calculatorCopm.find('.mainContainer')).toBeDefined();
+    expect(calculatorCopm.find('.resultRow')).toBeDefined();
+
 })
